45ba77ffd5c2bcd29d71a2d17dbee4e3343b403f
Hamsika <st26076@ormiston.school.nz>
2026-07-01 11:30:07 +1200

    ProjectStart

---
+++ app.py
@@ -0,0 +1,78 @@
+import json
+from flask import Flask, render_template, request, session, flash, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def index():
+    flowers, addons = load_data()
+    cart = session.get('cart', {})
+    total = calculate_total(cart)
+    return render_template('index.html', flowers=flowers, addons=addons, cart=cart, total=total)
+
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+    with open('data/addons.json') as file:
+        addons = json.load(file)
+    return flowers, addons
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    flower = request.form['flower']
+    quantity = int(request.form['quantity'])
+    flowers, addons = load_data()
+    cart = session.get('cart', {})
+
+    if flower not in flowers:
+        flash("Invalid flower selected.")
+        return redirect(url_for('index'))
+    
+    if flower in cart:
+        cart[flower]['quantity'] += quantity
+
+    else:
+        cart[flower] = {
+            'price': flowers[flower]['price'],
+            'quantity': quantity
+        }
+
+    session['cart'] = cart
+    session.modified = True
+    flash(f"{quantity} {flower}(s) added to cart.")
+    return redirect(url_for('index'))
+
+
+@app.route('/remove_from_cart/<item>')
+def remove_from_cart(item):
+    cart = session.get('cart', {})
+
+    if item in cart:
+        del cart[item]
+        session['cart'] = cart
+        session.modified = True
+        flash(f"Removed all {item.capitalize()} from cart.")
+    else:
+        flash("Item not found in cart")
+
+    return redirect(url_for('index'))
+
+def calculate_total(cart):
+    total = sum (item['price'] * item['quantity'] for item in cart.values())
+    return total
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/order_history')
+def order_history():
+    return render_template('order_history.html')
+
+@app.route('/invoices')
+def invoices():
+    return render_template('invoices.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
